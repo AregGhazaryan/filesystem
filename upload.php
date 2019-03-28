ca7fd783512ea53f56ae2a8ed84c11d74127ade3
Areg <areg.areg51@gmail.com>
2019-03-28 17:14:12 +0400

    fixed file upload same file case rename functionality

--- upload.php
+++ upload.php
@@ -18,17 +18,33 @@ if (isset($_POST['upload'])) {
     } else {
         //checking if subfolder directory exists
         if (empty($location) || !isset($location) || $location === "" || $location === "uploads/") {
-          $explode = explode(".", $fileName);
-          $extension = end($explode);
-          $fileactualname = reset($explode);
+          preg_match_all('/[A-Za-z0-1]*\.[a-zA-Z0-1]*/', $fileName, $matches);
+          $ext = end($matches);
+          $removed = array_pop($matches);
+          array_pop($removed);
+          $kek = implode($removed);
+          $deez = end($ext);
+
           $i = 0;
           $fn = $fileName;
-          while (file_exists("uploads/".$fn)) {
+          if (count($removed)<=1) {
+            $exp = explode(".",$fileName);
+            $end = end($exp);
+            while (file_exists("uploads/".$fn)) {
+                $i++;
+                $fn = reset($exp)."_".$i.".".$end;
+            }
+            $name = $fn;
+            $moveResult = move_uploaded_file($fileTmpLoc, $location . "/". $name);
+          }else{
+            while (file_exists("uploads/".$fn)) {
               $i++;
-              $fn = $i."_".$fileName;
+              // add number to actual filename
+              $fn = $kek."_".$i.$deez;
+            }
+            $name = $fn;
+            $moveResult = move_uploaded_file($fileTmpLoc, "uploads/". $name);
           }
-          $name = $fn;
-          $moveResult = move_uploaded_file($fileTmpLoc, "uploads/". $name);
             if ($moveResult != true) {
                 @unlink($fileTmpLoc);
                 header('location: ' . URL . '?page=0&sort=name&type=desc&folder=uploads/&message=uploaderror');
@@ -38,21 +54,33 @@ if (isset($_POST['upload'])) {
             if (!is_dir("uploads/".$location)) {
                 header('location: ' . URL . '?page=0&sort=name&type=desc&folder=uploads/&message=FDE');
             }
-            $explode = explode(".", $fileName);
-            $extension = end($explode);
-            $fileactualname = reset($explode);
+            preg_match_all('/[A-Za-z0-1]*\.[a-zA-Z0-1]*/', $fileName, $matches);
+            $ext = end($matches);
+            $removed = array_pop($matches);
+            array_pop($removed);
+            $kek = implode($removed);
+            $deez = end($ext);
 
             $i = 0;
             $fn = $fileName;
-
-            while (file_exists($location.$fn)) {
+            if (count($removed)<=1) {
+              $exp = explode(".",$fileName);
+              $end = end($exp);
+              while (file_exists($location.$fn)) {
+                  $i++;
+                  $fn = reset($exp)."_".$i.".".$end;
+              }
+              $name = $fn;
+              $moveResult = move_uploaded_file($fileTmpLoc, $location . "/". $name);
+            }else{
+              while (file_exists($location.$fn)) {
                 $i++;
                 // add number to actual filename
-                $fn = $i."_".$fileName;
+                $fn = $kek."_".$i.$deez;
+              }
+              $name = $fn;
+              $moveResult = move_uploaded_file($fileTmpLoc, $location . "/". $name);
             }
-
-            $name = $fn;
-            $moveResult = move_uploaded_file($fileTmpLoc, $location . "/". $name);
             if ($moveResult != true) {
                 @unlink($fileTmpLoc);
                 header('location: ' . URL . '?page=0&sort=name&type=desc&folder=uploads/&message=uploaderror');
